229d8b6468a7a02b965d63e97a3b95127cb013d0
sara <sarahkungfu32@gmail.com>
2026-03-30 00:19:24 +0100

    30.03 changes

--- fundamentos/AlunoOperadoresAritmeticos.cs
+++ fundamentos/AlunoOperadoresAritmeticos.cs
@@ -1,3 +1,5 @@
+using System.Security.AccessControl;
+
 namespace Fundamentos01;
 
 public class AlunoOperadoresAritmeticos
@@ -32,6 +34,40 @@ public void Executar()
     double altura = 6.0;
 
      Console.WriteLine($"Area do triangulo e: {largura} x {altura} = {largura * altura}\n");
+
+     //2.2 -> ask values to the user
+
+    Console.WriteLine("Por favor insira a largura \n");
+
+    double largura1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Por favor insira a altura \n");
+
+    double altura1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine($"Area do triangulo e: {largura1} x {altura1 } = {largura1 * altura1}\n");
+
+    Console.WriteLine("=========================================\n");
+
+    Console.WriteLine("EXERCÍCIO 3: Operador de modulo(%)"); 
+    
+    Console.WriteLine("=========================================\n");
+
+
+  Console.WriteLine("Por favor insira o num1 \n");
+
+    double numb1= Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Por favor insira o num2 \n");
+
+    double numb2 = Convert.ToDouble(Console.ReadLine());
+
+//int resto = ConvertToDouble(numb1 % numb2);
+
+    Console.WriteLine($"O resto da divisao e: {numb1} % {numb2 } = {numb1 % numb2}\n");
+
+   
+
 
     }
 
